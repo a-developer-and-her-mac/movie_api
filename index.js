b83aa96b627ec06a1d03eb9de436e4915235c111
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-07-07 08:28:12 -0700

    Added endpoints

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express'),
 const app = express();
 
 // List of top ten movies
-let topTenMovies = [
-  {},
+let topTenMovies = [{},
   {},
   {},
   {},
@@ -23,10 +22,64 @@ app.get('/', (req, res) => {
   res.send('Welcome to faveFlix!');
 });
 
-app.get('/movies', (req, res) => {
+app.get('/movies/top', (req, res) => {
   res.json(topTenMovies);
 });
 
+
+//Get a list of data about all movies
+
+app.get('/movies', (req, res) => {
+  res.send('Successfull GET request returning data on all movies.');
+});
+
+//Get data about a single movie, by title
+
+app.get('/movies/title', (req, res) => {
+  res.send('Successfull GET request returning data about a single movie by name.');
+});
+
+// Get data about a genre by title
+
+app.get('/movies/title/genre', (req, res) => {
+  res.send('Successfull GET request returning data about a genre by title.');
+});
+
+// Get data about a director by name
+
+app.get('/movies/director', (req, res) => {
+  res.send('Successfull GET request returning data about a director by name.');
+});
+
+// Post new users
+
+app.post('/users', (req, res) => {
+  res.send('Successfull POST request adding a new user.');
+});
+
+// Update user info
+
+app.put('/users/name/info', (req, res) => {
+  res.send('Successfull PUT request updating user info.');
+});
+
+// Update users list of favorite movies
+
+app.put('/users/name/favorites', (req, res) => {
+  res.send('Successfull PUT request to update list of users favorite movies.');
+});
+
+//Delete a movie from the users list of favorites
+
+app.delete('/users/name/favorites', (req, res) => {
+  res.send('Successfull DELETE request removing a movie from the users list of favorites.');
+});
+
+// Deregister a user
+
+app.delete('/users/name', (req, res) => {
+  res.send('Successfull DELETE request removing a user');
+});
 
 app.use(express.static('public'));
 
@@ -38,5 +91,5 @@ app.use((err, req, res, next) => {
 
 // Listen for requests
 app.listen(8080, () => {
-console.log('Your app is listening on port 8080.');
+  console.log('Your app is listening on port 8080.');
 });
