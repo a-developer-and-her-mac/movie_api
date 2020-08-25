cb6deeae6ac4cc8baa787c528951a9ac7c38ecfb
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2020-08-25 11:21:31 -0700

    updates

--- models.js
+++ models.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose'),
-      bcrypt = require('bcrypt');
+  bcrypt = require('bcrypt');
 
 //Movie Schema
 let movieSchema = mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
+  Title: {
+    type: String,
+    required: true
+  },
+  Description: {
+    type: String,
+    required: true
+  },
   Genre: {
     Name: String,
     Description: String
@@ -21,18 +27,30 @@ let movieSchema = mongoose.Schema({
 
 //User Schema
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: {
+    type: String,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
   Birthday: Date,
-  FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  FavoriteMovies: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Movie'
+  }]
 });
 
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
